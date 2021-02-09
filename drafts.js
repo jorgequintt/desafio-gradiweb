ef45d62bb1dc3e00b4b45ed03ff09be43bfc4066
jq <jorgequintt@gmail.com>
2021-02-09 17:26:19 -0500

    repository init

---
+++ drafts.js
@@ -0,0 +1,21 @@
+/**
+ * Infrastructura
+ * - Un server para escuchar API requests?
+ * - Servir los archivos estaticos
+ * -- Generar archivos publicos con webpack
+ * --- SASS
+ * --- React (simple)
+ */
+
+/*
+Bogota data endpoint:
+https://api.openweathermap.org/data/2.5/onecall?lat=4.7110&lon=-74.063644&exclude=minutely,hourly,alerts&appid={API_KEY}
+
+Paris endpoint:
+https://api.openweathermap.org/data/2.5/weather?q=Paris&appid={API_KEY} 
+
+=============================================
+
+ToDo:
+    * Set up webpack with SASS and React (Babel)
+*/
